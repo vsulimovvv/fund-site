786dfc815595d5fb485647f816245081af4d9765
vsulimovvv <super.sulimov2014@yandex.ru>
2022-09-27 14:03:28 +0300

    новая версия

---
+++ README.md
@@ -0,0 +1,15 @@
+# Система добра
+
+### Страницы
+
+Главная: https://vsulimovvv.github.io/fund-site/app/index.html  
+О компании: https://vsulimovvv.github.io/fund-site/app/about.html  
+Контакты: https://vsulimovvv.github.io/fund-site/app/contacts.html  
+Новости: https://vsulimovvv.github.io/fund-site/app/news.html  
+Проекты: https://vsulimovvv.github.io/fund-site/app/projects.html  
+Как помочь: https://vsulimovvv.github.io/fund-site/app/how-help.html  
+Если нужна помощь: https://vsulimovvv.github.io/fund-site/app/help.html  
+Фотоотчеты: https://vsulimovvv.github.io/fund-site/app/photo-report.html  
+Фотоотчеты 2: https://vsulimovvv.github.io/fund-site/app/photo-report-2.html  
+Описание проекта: https://vsulimovvv.github.io/fund-site/app/how-help.html  
+Описание новости: https://vsulimovvv.github.io/fund-site/app/descr-news.html
---
+++ app/js/main.js
@@ -0,0 +1,213 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // * ===== Mask input
+  $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
+  (function customSelect() {
+    const element = document.querySelectorAll('select');
+    element.forEach((el) => {
+      const choices = new Choices(el, {
+        itemSelectText: '',
+        searchEnabled: false,
+      });
+    });
+  })();
+
+  (function fixedHeader() {
+    function scrollHeader() {
+      const nav = document.querySelector('header');
+      if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+      } else {
+        nav.classList.remove('scroll-header');
+      }
+    }
+
+    window.addEventListener('scroll', scrollHeader);
+
+    // ! Change
+    function changeBg() {
+      const header = document.querySelector('header');
+      if (window.pageYOffset >= 90) {
+        header.classList.add('scroll-header');
+      }
+    }
+
+    changeBg();
+  })();
+
+  (function loadForm() {
+    let inputs = document.querySelectorAll('.input-file-upload');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.previousElementSibling,
+        labelVal = label.querySelector('.custom-file-upload span').innerText;
+
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+
+        if (countFiles)
+          label.querySelector('.custom-file-upload span').innerText =
+            'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.custom-file-upload span').innerText = labelVal;
+      });
+    });
+  })();
+
+  (function modals() {
+    function bindModal(openBtn, modal, close) {
+      const openBtnEl = document.querySelectorAll(openBtn);
+      const modalEl = document.querySelector(modal);
+      const closeEl = document.querySelectorAll(close);
+      const body = document.querySelector('body');
+      if (modalEl) {
+        openBtnEl.forEach((el) => {
+          el.addEventListener('click', (e) => {
+            if (e.target) {
+              e.preventDefault();
+            }
+            modalEl.classList.add('active');
+            body.classList.add('no-scroll');
+          });
+        });
+        closeEl.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          });
+        });
+        modalEl.addEventListener('click', (e) => {
+          if (e.target === modalEl) {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          }
+        });
+      }
+    }
+    bindModal('.btn-volunteer', '.popup--volunteer', '.popup__close');
+  })();
+
+  (function showMenu() {
+    const menuBtn = document.querySelector('.header__toggle');
+    const menu = document.querySelector('.mobile-menu');
+    const menuCloseBtn = document.querySelector('.mobile-menu__close');
+    const body = document.querySelector('body');
+    const overlay = document.querySelector('.overlay');
+
+    menuBtn.addEventListener('click', (e) => {
+      menu.classList.toggle('active');
+      overlay.classList.toggle('active');
+      body.classList.toggle('no-scroll');
+    });
+
+    overlay.addEventListener('click', (e) => {
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+      body.classList.remove('no-scroll');
+    });
+
+    menuCloseBtn.addEventListener('click', (e) => {
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+      body.classList.remove('no-scroll');
+    });
+  })();
+
+  (function projects() {
+    const slider = document.querySelectorAll('.projects-box__slider');
+    slider.forEach((el) => {
+      const swiper = new Swiper(el, {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: el.querySelector('.swiper-button-next'),
+          prevEl: el.querySelector('.swiper-button-prev'),
+        },
+      });
+    });
+  })();
+
+  (function collectedSlider() {
+    const slider = document.querySelectorAll('.collected__slider');
+    slider.forEach((el) => {
+      const swiper = new Swiper(el, {
+        slidesPerView: 1,
+        pagination: {
+          el: '.collected__content .swiper-pagination',
+          clickable: true,
+        },
+      });
+    });
+  })();
+
+  (function sliderNews() {
+    const slider = document.querySelectorAll('.news-slider__slider');
+    slider.forEach((el) => {
+      const swiper = new Swiper(el, {
+        slidesPerView: 'auto',
+        spaceBetween: 15,
+        pagination: {
+          el: '.news-slider__slider .swiper-pagination',
+          type: 'progressbar',
+        },
+        breakpoints: {
+          991: {
+            spaceBetween: 40,
+          },
+        },
+      });
+    });
+  })();
+
+  function tabs(headerSelector, tabSelector, contentSelector, activeClass) {
+    const header = document.querySelectorAll(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const content = document.querySelectorAll(contentSelector);
+
+    header.forEach((el) => {
+      if (el) {
+        hideTabContent();
+        showTabContent();
+        function hideTabContent() {
+          content.forEach((item) => {
+            item.classList.remove('active');
+          });
+          tab.forEach((item) => {
+            item.classList.remove(activeClass);
+          });
+        }
+        function showTabContent(i = 0) {
+          content[i].classList.add('active');
+          tab[i].classList.add(activeClass);
+        }
+        header.forEach((item) => {
+          if (item) {
+            item.addEventListener('click', (e) => {
+              const target = e.target;
+              if (target.classList.contains(tabSelector.replace(/\./, ''))) {
+                tab.forEach((item, i) => {
+                  if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+  tabs(
+    '.donation-block__top',
+    '.donation-block__btn',
+    '.donation-block__content',
+    'active'
+  );
+  tabs(
+    '.tabs-projects__nav',
+    '.tabs-projects__nav-btn',
+    '.tabs-projects__panel',
+    'active'
+  );
+});
